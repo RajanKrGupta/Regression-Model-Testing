506311af1616a53e96a13853aac52db9f994e534
RajanKrGupta <rajan.krgupta1984@gmail.com>
2023-12-08 17:40:17 +0530

    new testcases added

--- functions.py
+++ functions.py
@@ -18,13 +18,13 @@ def load_Empdata_df(filename):
     return df
 
 
-# Split Dataframe
+# Split Dataframe into train and test dataframe with columns id removed 
 def split_dataframe():
     df = load_Empdata_df(filename='Train-employee-salary.csv')
     # Mapping categorical values to numeric values
     blood_type_mapping = {'A': 0, 'B': 1, 'AB': 2, 'O': 3}
     df['groups'] = df['groups'].map(blood_type_mapping)
-    X = df.drop(columns='salary', axis=1)
+    X = df.drop(columns=['id','salary'], axis=1)  # removed ID and Salary columns
     y = df['salary']
     # Split the Test data into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -34,9 +34,9 @@ def split_dataframe():
     df_test = pd.concat([X_test, y_test], axis=1)
     return df_train, df_test
 
-#Converting Full Dataframe to full Dataset for Data integrity check via Deepcheck
+#Converting Full Dataframe to full Dataset for Data integrity check used in Deepchecks library
 def load_dataset():
-    df_train, df_test = split_dataframe()
+    df_train, df_test = split_dataframe()  
     # Mapping categorical values to numeric values
 
     # Create Dataset objects from dataframe for deepcheck usage
@@ -49,7 +49,6 @@ def load_dataset():
 def train_linear_model(filename='trained_SalaryPrediction_linear_model'):
     try:
         df_train, df_test = split_dataframe()
-
         # Check data types and valid test_frac value
         assert isinstance(df_train, pd.DataFrame), "X_train must be a DataFrame"
         assert isinstance(df_test, pd.DataFrame), "X_train must be a DataFrame"
@@ -62,6 +61,8 @@ def train_linear_model(filename='trained_SalaryPrediction_linear_model'):
         X_test = df_test.drop('salary',axis=1)
         y_train = df_train['salary']
         y_test = df_test['salary']
+
+        # Linear Regression Model creation
         model = LinearRegression()
 
         # Fit the model with training data
@@ -70,6 +71,56 @@ def train_linear_model(filename='trained_SalaryPrediction_linear_model'):
         # Save the trained model to a file
         fname = filename + '.joblib'
         dump(model, fname)
+        return model
+   
+    except AssertionError as msg:
+        print(msg)
+        return msg
+    
+
+# Assuming 'model' is your trained regression model
+
+def evaluate_salary_prediction(input_features, actual_salary):
+    try:
+        model = train_linear_model()
+        # Prepare input features for prediction
+        input_data = [input_features]  # Assuming input_features is already a list
+
+        # Make prediction
+        predicted_salary = model.predict(input_data)[0]
+
+        # Calculate accuracy
+        accuracy = (1 - abs(predicted_salary - actual_salary) / actual_salary) * 100
+        return predicted_salary, accuracy
+    except AssertionError as msg:
+        print(msg)
+        return msg
+
+# Function Overloading , f the test file doesn't have the salary values, and  we need predict the salary for each row using your model
+def salary_prediction(input_features):
+    try:
+        model = train_linear_model()
+        # Prepare input features for prediction
+        input_data = [input_features]  # Assuming input_features is already a list
+        # Make prediction
+        predicted_salary = model.predict(input_data)[0]        
+        return predicted_salary
+    except AssertionError as msg:
+        print(msg)
+        return msg
+
+
+# Function to calculate and return R-squared scores
+def Rsquared_linear_model():
+    try:
+        df_train, df_test = split_dataframe()
+        model = train_linear_model()
+        
+        # Splitting dataframe into X and y
+        X_train = df_train.drop('salary', axis=1)
+        X_test = df_test.drop('salary', axis=1)
+        y_train = df_train['salary']
+        y_test = df_test['salary']
 
         # Compute R-squared scores for training and test data
         r2_train = model.score(X_train, y_train)
@@ -79,8 +130,8 @@ def train_linear_model(filename='trained_SalaryPrediction_linear_model'):
         print("Test R-squared:", r2_test)
 
         # Return scores in a dictionary
-        return {'Train-score': r2_train, 'Test-score': r2_test}
-
+        score = {'Train-score': r2_train, 'Test-score': r2_test}
+        return score
     except AssertionError as msg:
         print(msg)
         return msg
--- test_functions.py
+++ test_functions.py
@@ -1,37 +1,38 @@
 import pytest
 import os
 import re
-from functions import load_Empdata_df, model_evaluation_suite, split_dataframe, train_linear_model, data_integrity_check, train_test_dataset_validation
+import functions
+#from functions import Rsquared_linear_model, evaluate_salary_prediction, load_Empdata_df, model_evaluation_suite, split_dataframe, train_linear_model, data_integrity_check, train_test_dataset_validation
 
-# Test presenec of Training Empdata_df function
+# Test presence of Training Empdata_df function
 def test_load_Train_Empdata_df():
-    df = load_Empdata_df('Train-employee-salary.csv')
+    df = functions.load_Empdata_df('Train-employee-salary.csv')
     assert df is not None
     assert 'salary' in df
     assert len(df) > 0
     # Add more specific assertions as needed
 
-# Test presenec of Test Empdata_df function
+# Test presence of Test Empdata_df function
 def test_load_Train_Empdata_df():
-    df = load_Empdata_df('Test-employee-salary.csv')
+    df = functions.load_Empdata_df('Test-employee-salary.csv')
     assert df is not None
-    #assert 'salary' not in df
+    assert 'salary' in df.columns
     assert len(df) > 0
 
 
 # Test split_dataframe function
 def test_split_dataframe():
-    train_df, test_df = split_dataframe()
+    train_df, test_df = functions.split_dataframe()
     assert train_df is not None
     assert test_df is not None
     assert len(train_df) > 0
     assert len(test_df) > 0
     # Add more specific assertions as needed
 
-# Test train_linear_model function
-def test_train_linear_model():
-    test_df, test_df = split_dataframe()
-    result = train_linear_model()
+# Test train_linear_model function for R-squared scores for training and test data
+@pytest.mark.prediction
+def test_Rsquared_scores_train_test():
+    result = functions.Rsquared_linear_model()
     assert 'Train-score' in result
     assert 'Test-score' in result
     assert 0 <= result['Train-score'] <= 1
@@ -41,8 +42,9 @@ def test_train_linear_model():
     # Add more specific assertions as needed
 
 # Test data_integrity_check function
+@pytest.mark.skip(reason="Working Time Consuming")
 def test_data_integrity_check():
-    data_integrity_check()
+    functions.data_integrity_check()
     # You can add assertions for the result of data integrity check, e.g., check for specific issues
 
 # Additional custom test cases
@@ -53,6 +55,8 @@ def test_saved_model_file_exists():
 
 # Test if the HTML report for data integrity check exists
 # Test if the HTML report for data integrity check exists
+@pytest.mark.skip(reason="Working Time Consuming")
+@pytest.mark.report
 def test_data_integrity_report_exists():
     import os
     assert os.path.isfile('Train_data_integrity_report.html')
@@ -60,13 +64,70 @@ def test_data_integrity_report_exists():
 # To validate two data subsets
 # Comparing distributions across different train-test splits (e.g. before training a model or when splitting data for cross-validation)
 # Comparing a new data batch to previous data batche
+
+@pytest.mark.skip(reason="Working Time Consuming")
 def test_train_test_dataset_validation():
     # Implement assertions to check for specific issues reported in the data integrity report
-    train_test_dataset_validation()
+    functions.train_test_dataset_validation()
 
-
+@pytest.mark.skip(reason="Working Time Consuming")
 def test_model_evaluation_suite():
     # Implement assertions to check for specific issues reported in the data integrity report
-    model_evaluation_suite()
+    functions.model_evaluation_suite()
 
 # You can add more custom test cases as needed
+
+@pytest.mark.prediction
+def test_positive_salary_prediction(input_features=[0, 36, 5, 5], actual_salary = 2297 ):
+    predicted_salary, accuracy = functions.evaluate_salary_prediction(input_features, actual_salary)
+    print(f"Actual Salary: {actual_salary}")
+    print(f"Predicted Salary: {predicted_salary}")
+    print(f"Accuracy: {accuracy:.2f}%")
+    assert predicted_salary > 2000
+    assert 0< accuracy <100
+
+
+@pytest.mark.testdf_prediction
+def test_checking_predicted_salary_from_actual():
+    # Load input data from CSV
+    df_train, df_test = functions.split_dataframe()
+
+    # Iterate over rows in the CSV file
+    for index, row in df_test.iterrows():
+        input_features = [row['groups'], row['age'], row['healthy_eating'], row['active_lifestyle']]
+        actual_salary = row['salary']
+
+        # Perform salary prediction for each row
+        predicted_salary, accuracy = functions.evaluate_salary_prediction(input_features, actual_salary)
+
+        # Add assertions based on your requirements
+        assert accuracy > 0  # Add your specific assertions here
+
+        # Optionally, print results for each row
+        print(f"Row {index + 1} - Predicted Salary: {predicted_salary}, Actual Salary: {actual_salary}, Accuracy: {accuracy}%\n")
+
+
+@pytest.mark.new
+def test_salary_prediction_from_csv():
+    # Load input data from CSV
+    input_data = functions.load_Empdata_df('Test-employee-salary.csv')
+    input_data = input_data.drop(columns='id', axis=1)
+    
+     # Mapping categorical values to numeric values
+    blood_type_mapping = {'A': 0, 'B': 1, 'AB': 2, 'O': 3}
+    input_data['groups'] = input_data['groups'].map(blood_type_mapping)
+
+    # Iterate over rows in the CSV file
+    for index, row in input_data.iterrows():
+        input_features = [row['groups'], row['age'], row['healthy_eating'], row['active_lifestyle']]
+
+        # Perform salary prediction for each row
+        predicted_salary = functions.salary_prediction(input_features)
+
+        # Add assertions based on your requirements
+        assert predicted_salary is not None  # Add your specific assertions here
+
+        # Optionally, print results for each row
+        print(f"Row {index + 1} - Predicted Salary: {predicted_salary}")
+
+# ... other test cases ...
